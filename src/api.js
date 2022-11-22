b0e4598844da6eba65ac6f65ff7b6e0f75370f4b
belalgamal46 <belalgamal00@gmail.com>
2022-11-22 15:19:04 +0200

    Fix linters

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ export const getTrendingMovies = async () => {
 
 export const getGenres = async (id) => {
   const data = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=4de7975ef400d300ae019f76c489bf9f&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`,
+    `https://api.themoviedb.org/3/discover/movie?api_key=4de7975ef400d300ae019f76c489bf9f&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
   );
   const res = await data.json();
   return res.results;
