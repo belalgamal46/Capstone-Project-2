f59fa96eccb77b5a20e2e9c1a3007f3d4aa4dd69
belalgamal46 <belalgamal00@gmail.com>
2022-11-24 03:19:35 +0200

    Add display number of likes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
 import './styles.css';
-import { getTrendingMovies, getGenres } from './api.js';
+import { getTrendingMovies, getGenres } from './modules/api.js';
+import { getLikes, createLikes } from './modules/involvementAPI.js';
 
 const genres = document.querySelectorAll('.genres');
 const logo = document.querySelector('.logo');
 
+const getMovielikesData = async () => {
+  const likesData = await getLikes();
+  return likesData.reduce((prev, curr) => ({ ...prev, [curr.item_id]: curr }), {});
+};
+
+const moviesList = document.querySelector('.movies-list');
+
 const displayMovies = async (list, title = '') => {
-  const data = await list;
-  const moviesList = document.querySelector('.movies-list');
   const categoryTitle = document.querySelector('.title');
   categoryTitle.innerHTML = title;
   moviesList.innerHTML = '';
-  data.forEach((movie) => {
-    moviesList.innerHTML += `
-    <li>
+  const data = await list;
+  const likesDataObject = await getMovielikesData();
+  data.forEach(async ({ poster_path, name, title, id }) => {
+    const li = document.createElement('li');
+    li.classList.add('movie-item');
+    li.id = id;
+    li.innerHTML = `
       <div class="image-container">
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="poster" />
-      </div>
-      <div class="movie-title-container">
-        <h2>${movie.name || movie.title}</h2>
-        <button type="button" class="like-btn">
-          <span class="material-symbols-outlined"> favorite </span>
-          5 likes
-        </button>
+        <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="poster" />
       </div>
       <div>
-        <button type="button" class="comments-btn">Comments</button>
-      </div>
-    </li>
-    `;
+        <div class="movie-title-container">
+          <h2>${name || title}</h2>
+          <button type="button" class="like-btn">
+            <span class="material-symbols-outlined"> favorite </span>
+            <span>${likesDataObject[id]?.likes || 0} likes</span>
+          </button>
+        </div>
+        <div>
+          <button type="button" class="comments-btn">Comments</button>
+        </div>
+      </div>`;
+    moviesList.appendChild(li);
   });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   displayMovies(getTrendingMovies(), 'Trending');
 });
 
---
+++ src/modules/api.js
@@ -0,0 +1,24 @@
+const apiKey = '4de7975ef400d300ae019f76c489bf9f';
+const baseUrl = 'https://api.themoviedb.org/3';
+
+export const getTrendingMovies = async () => {
+  try {
+    const data = await fetch(`${baseUrl}/trending/all/day?api_key=${apiKey}`);
+    const dataParsed = await data.json();
+    return dataParsed.results;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getGenres = async (id) => {
+  try {
+    const data = await fetch(
+      `${baseUrl}/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
+    );
+    const res = await data.json();
+    return res.results;
+  } catch (error) {
+    return error;
+  }
+};
