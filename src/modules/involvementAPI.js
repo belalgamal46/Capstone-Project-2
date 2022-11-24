d698bcc49707e8611a5ec0a231ade1bff77c2dad
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2022-11-24 14:03:24 +0100

    Add comments feature

--- src/modules/involvementAPI.js
+++ src/modules/involvementAPI.js
@@ -1,5 +1,6 @@
 const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
 const likesListId = 'd7vELbAR59v1NLTIl9t7';
+const commentsId = '2kso98B7xYsjYtK8ZJ1x';
 
 export const getLikes = async () => {
   try {
@@ -20,6 +21,30 @@ export const createLikes = async (id) => {
         'Content-Type': 'application/json; charset=utf-8',
       },
     });
+  } catch (error) {
+    return error;
+  }
+};
+
+export const createComments = async (id, username, comment) => {
+  try {
+    return await fetch(`${baseUrl}/apps/${commentsId}/comments?item_id=${id}`, {
+      method: 'POST',
+      body: JSON.stringify({ item_id: id, username, comment }),
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    });
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getComments = async (id) => {
+  try {
+    const data = await fetch(`${baseUrl}/apps/${commentsId}/comments?item_id=${id}`);
+    const response = await data.json();
+    return response;
   } catch (error) {
     return error;
   }
